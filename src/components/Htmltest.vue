bb28cd9c01a35b9b8857d5de837f7c5ae9d2af45
bbaaag <b15737977957@126.com>
2019-03-27 23:28:10 +0800

    mac 第一次提交

---
+++ src/components/Htmltest.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <ul>
+            <li  @click="add" v-for="(item, index) in arr" :key="index" >{{item}}</li>
+        </ul>
+    </div>
+    
+    
+</template>
+
+<script>
+    export default {
+        name: '',
+        data() {
+            return {
+                arr : [1,2]
+            }
+        },
+        mounted() {
+            this.getData()
+        },
+        methods: {
+            getData () {
+                this.$axios.get('/api/goods') //easy-mock的模拟出来的接口
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+            add () {
+                this.arr[1]=0 ;
+                console.log(this.arr)
+            }
+        },  
+    }
+</script>
+
+<style lang="" scoped>
+    
+</style>
